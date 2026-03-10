4dbd01af0f6bb4dfb90e6891343b694b38f4d2ce
ayi12gsu <ayi12@student.gsu.edu>
2026-03-10 16:25:09 -0400

    Delete About.vue

--- About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <h1>About Page</h1>
-  <p>Created by Andrew Yi, Jasmine Saunders, and Mario Gutierrez for CSC4370 - Web Programming Final Project</p>
-</template>
-
-<style scoped></style>
